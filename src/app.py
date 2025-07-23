03fe8d6dffd91d46447cfa49d06d656dee8515e8
lucaNeves <luizneves6@hotmail.com>
2025-07-23 15:53:57 -0300

    inicicio do python-app

---
+++ src/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+
+app = Flask(__name__)
+
+
+# @app.route('/api/v1/info')
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname()
+    })
+# def info():
+#     return jsonify({
+#     	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+#     	'hostname': socket.gethostname(),
+#         'message': 'You are doing great, little human! <3',
+#         'deployed_on': 'kubernetes'
+#     })
+
+@app.route('/api/v1/health')
+
+def health():
+	# Do an actual check here
+    return jsonify({'status': 'up'}), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
+#'/api/v1/details'
+#'/api/v1/health'

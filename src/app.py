e2eeed83e88d308f9fff18dc20e9d06433793413
lucaNeves <luizneves6@hotmail.com>
2025-08-21 12:16:09 -0300

    new updates on ci/cd

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
     	'hostname': socket.gethostname(),
-        'message': 'You are doing great, little human! <3 !!'
+        'message': 'You are doing great, little human! <3 ;)'
     })
 # def info():
 #     return jsonify({
